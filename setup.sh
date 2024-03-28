d7c9e0fa3fb4d965540305494f957f2db41b2de2
muplagama <37286134+muplagama@users.noreply.github.com>
2024-03-28 14:44:33 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ wget -O /etc/oscam/oscam https://lou.h4ck.me/builds/testing/oscam-svn11748-arm_v
 chmod +x /etc/oscam/oscam
 mkdir /etc/oscam/config
 
+wget -O /etc/init.d/oscam https://raw.githubusercontent.com/muplagama/pi_oscam/main/rc 
