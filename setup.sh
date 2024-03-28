bfa06716d4bebfc816caaf6f84c9d6a1b1e65b62
muplagama <37286134+muplagama@users.noreply.github.com>
2024-03-28 14:42:54 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 mkdir /etc/oscam
 wget -O /etc/oscam/oscam https://lou.h4ck.me/builds/testing/oscam-svn11748-arm_v1403 > /dev/null 2>&1
+chmod +x /etc/oscam/oscam
+mkdir /etc/oscam/config
+
